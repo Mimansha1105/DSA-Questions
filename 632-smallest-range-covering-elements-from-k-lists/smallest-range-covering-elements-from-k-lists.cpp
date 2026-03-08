5f92ec195a3ea1b0cc35ed447043eadfc00ff995
Mimansha1105 <mimansha.clg@gmail.com>
2026-03-08 23:39:30 +0530

    Time: 107 ms (33.67%) | Memory: 51.3 MB (39.09%) - LeetSync

---
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,77 @@
+#include <limits.h>
+#include <queue>
+using namespace std;
+
+class node{
+public:
+    int data;
+    int row;
+    int col;
+
+    node(int d,int r,int c){
+        data=d;
+        row=r;
+        col=c;
+    }
+};
+
+class compare{
+public:
+    bool operator()(node* a,node* b){
+        return a->data > b->data;
+    }
+};
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+int k = nums.size();
+
+int mini = INT_MAX;
+int maxi = INT_MIN;
+
+ priority_queue<node*, vector<node*>, compare> minheap;
+
+      for(int i=0;i<k;i++){
+
+    int element = nums[i][0];
+
+            mini = min(mini, element);
+        maxi = max(maxi, element);
+
+            minheap.push(new node(element,i,0));
+        }
+
+        int start = mini;
+        int end = maxi;
+
+        while(true){
+      node* temp = minheap.top();
+      minheap.pop();
+
+            mini = temp->data;
+
+      
+            if(maxi - mini < end - start){
+                start = mini;
+                end = maxi;
+            }
+
+           
+            if(temp->col + 1 < nums[temp->row].size()){
+
+                int nextVal = nums[temp->row][temp->col + 1];
+
+                maxi = max(maxi, nextVal);
+
+                minheap.push(new node(nextVal,
+                     temp->row,
+                      temp->col + 1));
+            }
+            else{
+                break;
+            }
+        }
+
+        return {start, end};
+    }
+};
