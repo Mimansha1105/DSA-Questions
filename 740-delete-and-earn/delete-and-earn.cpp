fb1cd017c9ca41108ce67be4a609fdefca96f252
Mimansha1105 <mimansha.clg@gmail.com>
2026-04-22 13:11:06 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (97.50%) - LeetSync

---
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+int maxVal = 0;
+    for(int x : nums) maxVal = max(maxVal, x);
+    vector<int> points(maxVal + 1, 0);
+    for(int x : nums) {
+        points[x] += x;
+    }
+    int prev2 = 0;          
+    int prev1 = points[0];  
+    for(int i = 1; i <= maxVal; i++) {
+        int take = points[i] + prev2;
+        int skip = prev1;
+        int curr = max(take, skip);
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return prev1;
+    }
+};
