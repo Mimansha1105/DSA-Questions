724eb18550faa481848a432de184d144c6851350
Mimansha1105 <mimansha.clg@gmail.com>
2026-04-29 21:49:04 +0530

    Time: 3 ms (67.41%) | Memory: 12.2 MB (46.26%) - LeetSync

---
+++ 133-clone-graph/clone-graph.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    unordered_map<Node*, Node*> mp;
+    Node* cloneGraph(Node* node) {
+        if (!node) return NULL;
+        if (mp.find(node) != mp.end()) {
+            return mp[node];
+        }
+        Node* clone = new Node(node->val);
+        mp[node] = clone;
+        for (auto neighbor : node->neighbors) {
+            clone->neighbors.push_back(cloneGraph(neighbor));
+        }
+        return clone;
+    }
+};
